0b961b367ae79542c8102f27b3df66be2f054aee
Ivan <contaivanrodrigues@gmail.com>
2026-06-05 21:09:49 -0300

    feat: add handle download to image viewer

--- client/src/components/ImageViewer.jsx
+++ client/src/components/ImageViewer.jsx
@@ -6,10 +6,35 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import DownloadIcon from "@mui/icons-material/Download";
 
 export function ImageViewer({ image, onClose }) {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleDownload = async () => {
+    try {
+      const response = await fetch(image, { mode: "cors" });
+      if (!response.ok) throw new Error("Erro ao baixar imagem");
+
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = image.split("/").pop()
+        ? image.split("/").pop()
+        : "image.jpg";
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Dialog
@@ -19,6 +44,17 @@ export function ImageViewer({ image, onClose }) {
       fullWidth={isMobile ? true : false}
     >
       <Box display="flex" justifyContent="center">
+        <IconButton
+          onClick={handleDownload}
+          color="primary"
+          sx={{
+            position: "absolute",
+            top: 10,
+            right: 60,
+          }}
+        >
+          <DownloadIcon />
+        </IconButton>
         <IconButton
           onClick={onClose}
           color="primary"
